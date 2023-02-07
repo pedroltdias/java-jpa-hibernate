8e421a8eeceecbfa7e1acd98205fe5b7c9095fdf
Pedro Dias <pedroluksteixeira@gmail.com>
2023-02-07 10:58:28 -0300

    Feat: Categoria relation with Produto (ManyToOne)

--- src/main/java/testes/CadastroDeProduto.java
+++ src/main/java/testes/CadastroDeProduto.java
@@ -1,27 +1,30 @@
 package testes;
 
+import br.com.alura.loja.dao.CategoriaDAO;
 import br.com.alura.loja.dao.ProdutoDAO;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Produto celular = new Produto();
-        celular.setNome("Xiaomi Redmi");
-        celular.setDescricao("Muito legal");
-        celular.setPreco(new BigDecimal("800"));
+        Categoria celulares = new Categoria("Celulares");
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal",
+                new BigDecimal("800"), celulares);
 
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDAO dao = new ProdutoDAO(em);
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 
         em.getTransaction().begin();
-        dao.cadastrar(celular);
+
+        categoriaDAO.cadastrar(celulares);
+        produtoDAO.cadastrar(celular);
+
         em.getTransaction().commit();
         em.close();
     }
