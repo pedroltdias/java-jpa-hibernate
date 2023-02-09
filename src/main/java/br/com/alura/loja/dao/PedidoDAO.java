d334fa27252d77ba7395d04f9ceaaca7a02f46e4
Pedro Dias <pedroluksteixeira@gmail.com>
2023-02-09 10:50:39 -0300

    Feat: Using 'RelatorioDeVendasVo' to type the query

--- src/main/java/br/com/alura/loja/dao/PedidoDAO.java
+++ src/main/java/br/com/alura/loja/dao/PedidoDAO.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.dao;
 
 import br.com.alura.loja.modelo.Pedido;
+import br.com.alura.loja.vo.RelatorioDeVendasVo;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
@@ -24,14 +25,17 @@ public class PedidoDAO {
                 .getSingleResult();
     }
 
-    public List<Object[]> relatorioDeVendas() {
-        String jpql = "SELECT produto.nome, SUM(item.quantidade), MAX(pedido.data) " +
+    public List<RelatorioDeVendasVo> relatorioDeVendas() {
+        String jpql = "SELECT new br.com.alura.loja.vo.RelatorioDeVendasVo( " +
+                "produto.nome, " +
+                "SUM(item.quantidade), " +
+                "MAX(pedido.data)) " +
                 "FROM Pedido pedido " +
                 "JOIN pedido.itens item " +
                 "JOIN item.produto produto " +
                 "GROUP BY produto.nome " +
                 "ORDER BY item.quantidade DESC";
-        return em.createQuery(jpql, Object[].class)
+        return em.createQuery(jpql, RelatorioDeVendasVo.class)
             .getResultList();
     }
 }
--- src/main/java/testes/CadastroDePedido.java
+++ src/main/java/testes/CadastroDePedido.java
@@ -6,6 +6,7 @@ import br.com.alura.loja.dao.PedidoDAO;
 import br.com.alura.loja.dao.ProdutoDAO;
 import br.com.alura.loja.modelo.*;
 import br.com.alura.loja.util.JPAUtil;
+import br.com.alura.loja.vo.RelatorioDeVendasVo;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
@@ -21,34 +22,43 @@ public class CadastroDePedido {
         ClienteDAO clienteDAO = new ClienteDAO(em);
 
         Produto produto = produtoDAO.buscarPorId(1l);
+        Produto produto2 = produtoDAO.buscarPorId(2l);
+        Produto produto3 = produtoDAO.buscarPorId(3l);
         Cliente cliente = clienteDAO.buscarPorId(1l);
 
         em.getTransaction().begin();
 
         Pedido pedido = new Pedido(cliente);
         pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+        pedido.adicionarItem(new ItemPedido(40, pedido, produto2));
+
+        Pedido pedido2 = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(2, pedido, produto3));
 
         PedidoDAO pedidoDAO = new PedidoDAO(em);
         pedidoDAO.cadastrar(pedido);
+        pedidoDAO.cadastrar(pedido2);
 
         em.getTransaction().commit();
 
         BigDecimal totalVendido = pedidoDAO.valorTotalVendido();
         System.out.println("Total Vendido: " + totalVendido);
 
-        List<Object[]> relatorio = pedidoDAO.relatorioDeVendas();
-        for (Object[] obj : relatorio) {
-            System.out.println(obj[0]);
-            System.out.println(obj[1]);
-            System.out.println(obj[2]);
-        }
+        List<RelatorioDeVendasVo> relatorio = pedidoDAO.relatorioDeVendas();
+        relatorio.forEach(System.out::println);
 
     }
 
     private static void popularBD() {
-        Categoria celulares = new Categoria("Celulares");
+        Categoria celulares = new Categoria("CELULARES");
+        Categoria videogames = new Categoria("VIDEOGAMES");
+        Categoria informatica = new Categoria("INFORMATICA");
+
         Produto celular = new Produto("Xiaomi Redmi", "Muito legal",
                 new BigDecimal("800"), celulares);
+        Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("5000"), videogames);
+        Produto macbook = new Produto("Macbook", "Macbook Dev", new BigDecimal("10000"), informatica);
+
 
         Cliente cliente = new Cliente("Cliente", "123456");
 
@@ -60,7 +70,13 @@ public class CadastroDePedido {
         em.getTransaction().begin();
 
         categoriaDAO.cadastrar(celulares);
+        categoriaDAO.cadastrar(videogames);
+        categoriaDAO.cadastrar(informatica);
+
         produtoDAO.cadastrar(celular);
+        produtoDAO.cadastrar(videogame);
+        produtoDAO.cadastrar(macbook);
+
         clienteDAO.cadastrar(cliente);
 
         em.getTransaction().commit();
